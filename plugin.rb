17b0291e937fd6125eb5d66ff48aee44ee3b41ad
joo2017 <30431754+joo2017@users.noreply.github.com>
2025-08-19 23:34:18 +0900

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -6,8 +6,8 @@
 # authors: [Your Name]
 # url: [Your GitHub Repo URL]
 
-# 注册并加载我们的站点配置文件
-load File.expand_path('config/settings.yml', __dir__)
+# 核心修正：我们已经删除了错误的 `load` 语句。
+# Discourse会自动加载 config/settings.yml 文件。
 
 enabled_site_setting :lottery_enabled
 
