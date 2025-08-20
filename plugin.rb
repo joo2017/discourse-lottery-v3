d7dd13067dac8fc36032d48dcd2d87b3bc8d1cbc
joo2017 <30431754+joo2017@users.noreply.github.com>
2025-08-20 13:03:53 +0900

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
-# file: discourse-lottery-v3/plugin.rb (最终修正版)
+# file: discourse-lottery-v3/plugin.rb
 
-# name: Discourse Lottery V3
+# name: discourse-lottery-v3
 # about: A comprehensive and robust lottery plugin for Discourse, based on the V3 blueprint.
 # version: 0.1
 # authors: [Your Name]
@@ -9,5 +9,5 @@
 enabled_site_setting :lottery_enabled
 
 after_initialize do
-  # ... 所有核心的后端逻辑将从这里开始挂载 ...
+  # All server-side logic will go here.
 end
