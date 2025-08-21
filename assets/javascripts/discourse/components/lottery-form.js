3d2d1233a97f8293ad78eda560df87e737d04cb8
joo2017 <30431754+joo2017@users.noreply.github.com>
2025-08-21 21:16:26 +0900

    Update lottery-form.js

--- assets/javascripts/discourse/components/lottery-form.js
+++ assets/javascripts/discourse/components/lottery-form.js
@@ -1,5 +1,4 @@
 // file: discourse-lottery-v3/assets/javascripts/discourse/components/lottery-form.js
-
 import Component from "@glimmer/component";
 import { tracked } from "@glimmer/tracking";
 import { action } from "@ember/object";
@@ -11,14 +10,9 @@ export default class LotteryForm extends Component {
   get shouldShow() {
     const s = this.args.siteSettings;
     const m = this.args.model;
-
-    // 所有关键对象判空
     if (!s?.lottery_enabled || !m) return false;
     const allowedCats = (s.lottery_allowed_categories || "")
-      .split("|")
-      .map(Number)
-      .filter(Boolean);
-
+      .split("|").map(Number).filter(Boolean);
     return (
       m.action === "createTopic" &&
       !!m.categoryId &&
@@ -27,11 +21,9 @@ export default class LotteryForm extends Component {
     );
   }
 
-  // 保证表单数据总是对象，所有字段都有默认值
   get lotteryData() {
     const m = this.args.model;
     if (!m.lotteryFormData) m.lotteryFormData = {};
-
     let data = m.lotteryFormData;
     if (!("prize_name" in data)) data.prize_name = "";
     if (!("prize_details" in data)) data.prize_details = "";
@@ -40,7 +32,6 @@ export default class LotteryForm extends Component {
     if (!("specified_post_numbers" in data)) data.specified_post_numbers = "";
     if (!("min_participants" in data)) data.min_participants = this.args.siteSettings?.lottery_min_participants_global || 1;
     if (!("backup_strategy" in data)) data.backup_strategy = "continue";
-
     return data;
   }
 
@@ -49,24 +40,13 @@ export default class LotteryForm extends Component {
     { id: "cancel", name: I18n.t("lottery.form.backup_strategy.options.cancel") }
   ];
 
-  @action
-  updateLotteryData(field, event) {
-    this.lotteryData[field] = event.target.value;
-  }
-
-  @action
-  updateLotteryDataFromComponent(field, value) {
-    this.lotteryData[field] = value;
-  }
-
-  @action
-  validateMinParticipants(value) {
+  @action updateLotteryData(field, event) { this.lotteryData[field] = event.target.value; }
+  @action updateLotteryDataFromComponent(field, value) { this.lotteryData[field] = value; }
+  @action validateMinParticipants(value) {
     const s = this.args.siteSettings;
     this.lotteryData["min_participants"] = value;
     if (value && parseInt(value, 10) < (s.lottery_min_participants_global || 1)) {
       this.minParticipantsError = I18n.t("lottery.form.min_participants.error", { count: s.lottery_min_participants_global || 1 });
-    } else {
-      this.minParticipantsError = null;
-    }
+    } else { this.minParticipantsError = null; }
   }
 }
