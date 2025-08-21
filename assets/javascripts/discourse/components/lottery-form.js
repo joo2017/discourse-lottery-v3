1db1eb0b1507be3726e6a93432f48eb4d7787862
joo2017 <30431754+joo2017@users.noreply.github.com>
2025-08-21 19:25:09 +0900

    Update lottery-form.js

--- assets/javascripts/discourse/components/lottery-form.js
+++ assets/javascripts/discourse/components/lottery-form.js
@@ -8,6 +8,25 @@ import I18n from "discourse-i18n";
 export default class LotteryForm extends Component {
   @tracked minParticipantsError = null;
 
+  // 新增：一个 getter，用于判断组件自身是否应该显示
+  get shouldShow() {
+    const siteSettings = this.args.siteSettings;
+    if (!siteSettings.lottery_enabled) {
+      return false;
+    }
+
+    const allowedCategoriesSetting = siteSettings.lottery_allowed_categories || "";
+    const allowedCategories = allowedCategoriesSetting.split('|').map(Number).filter(id => id > 0);
+    
+    // 使用可选链操作符 ?. 来安全地访问属性，防止 model 不存在时报错
+    const composerAction = this.args.model?.action;
+    const currentCategoryId = this.args.model?.categoryId;
+
+    return composerAction === 'createTopic' &&
+           allowedCategories.length > 0 &&
+           allowedCategories.includes(currentCategoryId);
+  }
+
   backupStrategyOptions = [
     { id: 'continue', name: I18n.t('lottery.form.backup_strategy.options.continue') },
     { id: 'cancel', name: I18n.t('lottery.form.backup_strategy.options.cancel') }
@@ -15,7 +34,8 @@ export default class LotteryForm extends Component {
 
   constructor() {
     super(...arguments);
-    if (!this.args.model.lotteryFormData) {
+    // 只有在 model 存在时才初始化数据
+    if (this.args.model && !this.args.model.lotteryFormData) {
       this.args.model.lotteryFormData = {};
     }
   }
