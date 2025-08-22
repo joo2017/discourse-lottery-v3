6b33ba5367591052eb7a1defb1aa2052b63517dc
joo2017 <30431754+joo2017@users.noreply.github.com>
2025-08-22 10:01:48 +0900

    Update lottery-form.js

--- assets/javascripts/discourse/components/lottery-form.js
+++ assets/javascripts/discourse/components/lottery-form.js
@@ -1,12 +1,12 @@
-import Component from "@glimmer/component";
-import { tracked } from "@glimmer/tracking";
-import { action } from "@ember/object";
-
-export default class LotteryForm extends Component {
-  @tracked msg = "Hello, Discourse!";
-
-  @action
-  setMsg(e) {
-    this.msg = e.target.value;
-  }
-}
+// import Component from "@glimmer/component";
+// import { tracked } from "@glimmer/tracking";
+// import { action } from "@ember/object";
+//
+// export default class LotteryForm extends Component {
+//   @tracked msg = "Hello, Discourse!";
+//
+//   @action
+//   setMsg(e) {
+//     this.msg = e.target.value;
+//   }
+// }
