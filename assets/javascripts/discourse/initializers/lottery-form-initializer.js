6ca27a5ff66521087c8b5a1d478c4b4393aa75be
joo2017 <30431754+joo2017@users.noreply.github.com>
2025-08-22 12:09:11 +0900

    Create lottery-form-initializer.js

---
+++ assets/javascripts/discourse/initializers/lottery-form-initializer.js
@@ -0,0 +1,19 @@
+import { withPluginApi } from "discourse/lib/plugin-api";
+
+export default {
+  name: "lottery-form-initializer",
+  initialize() {
+    withPluginApi("1.0.0", (api) => {
+      api.modifyClass("controller:composer", {
+        pluginId: "discourse-lottery-v3",
+        save(options) {
+          const lotteryData = this.get("model.lotteryFormData");
+          if (lotteryData) {
+            this.get("model").set("custom_fields.lottery", JSON.stringify(lotteryData));
+          }
+          return this._super(options);
+        },
+      });
+    });
+  },
+};
